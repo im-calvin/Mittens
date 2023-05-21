81b44db340059de88d4538b12313ad2f6b6a5ea2
im-calvin <kelvinwong0519@gmail.com>
2023-05-21 12:16:39 -0700

    update docker-compose

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import { AppDataSource } from "./db/data-source.js";
 export const targetConfidence = 0.7; // the confidence to be greater than to translate the message [0, 1]
 export const targetLanguage = ["ja"]; // possible languages to translate from (ISO 691)
 
-export const intervalTime = 10; // the interval for scraping
+export const intervalTime = 5; // the interval for scraping
 
 export async function getDBStreamers(): Promise<Streamer[]> {
   const transaction = Sentry.startTransaction({
