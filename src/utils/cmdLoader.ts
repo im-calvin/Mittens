37790e03deed2ff24d4b16dc5a4036d4334ca249
im-calvin <kelvinwong0519@gmail.com>
2023-05-24 01:55:18 -0700

    rf cmdLoader

--- src/utils/cmdLoader.ts
+++ src/utils/cmdLoader.ts
@@ -18,33 +18,27 @@ export const commands: CommandData[] = [add, remove, list];
 
 export async function autoCompleteStreamers(interaction: AutocompleteInteraction) {
   const focusedValue = interaction.options.getString("streamer", true).toLowerCase();
+
+  if (focusedValue === undefined || focusedValue === "") return;
+
   const streamers = await getDBStreamers();
   const orgs = await getGroups();
+  let filtered;
 
   // if the focusedValue is an org, return all streamers in that org
-  if (orgs.map((org) => org.name).includes(focusedValue)) {
-    const org = orgs.find((org) => org.name === focusedValue);
-    if (org === undefined) {
-      throw new Error("Org is undefined, but it was found in the list of orgs");
-    }
-    const response = org.streamers
-      .map((streamer) => ({
-        name: streamer.name,
-        value: streamer.id,
-      }))
-      .slice(0, 25);
-    await interaction.respond(response);
-    return;
-  } else if (streamers.map((streamer) => streamer.name.toLowerCase()).includes(focusedValue)) {
+  if (orgs.map((org) => org.name.toLowerCase().includes(focusedValue))) {
+    filtered = orgs.filter((org) => org.name.includes(focusedValue));
+  } else if (streamers.map((streamer) => streamer.name.toLowerCase().includes(focusedValue))) {
     // if the focusedValue is a streamer, return that streamer
-    const filtered = streamers.filter((s) => s.name.toLowerCase().includes(focusedValue));
-    const response = filtered
-      .map((streamer) => ({
-        name: streamer.name,
-        value: streamer.id,
-      }))
-      .slice(0, 25);
-    await interaction.respond(response);
+    filtered = streamers.filter((s) => s.name.toLowerCase().includes(focusedValue));
+  } else {
+    throw new Error("No streamer or org found");
   }
-  throw new Error("No streamer or org found");
+  const response = filtered
+    .map((streamer) => ({
+      name: streamer.name,
+      value: streamer.id,
+    }))
+    .slice(0, 25);
+  await interaction.respond(response);
 }
