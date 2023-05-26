b6a9ffb10985904bbfa95163fe88b4af97ca85d5
im-calvin <kelvinwong0519@gmail.com>
2023-05-25 22:59:13 -0700

    add messageCache to Client to edit translation message

--- src/utils/Client.ts
+++ src/utils/Client.ts
@@ -5,6 +5,7 @@ import { readEnv } from "./env.js";
 export default class MittensClient extends Client {
   #rest: REST = new REST().setToken(readEnv("DISCORD_TOKEN"));
   commands = new Collection<string, CommandData>();
+  messageCache = new Map<string, string>(); // used for translation (user msg id: bot msg id)
   constructor(options: ClientOptions) {
     super(options);
     commands.forEach((c) => this.addToCollection(c));
