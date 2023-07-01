668de04d84d1fb537e09d76063b0a1c1efa0632c
im-calvin <kelvinwong0519@gmail.com>
2023-07-01 00:05:42 -0700

    add LivePinged migration; not finished yet

---
+++ src/db/migration/1688194621864-AddLivePinged.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddLivePinged1688194621864 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE videos ADD live_pinged boolean`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE videos DROP COLUMN live_pinged`);
+  }
+}
