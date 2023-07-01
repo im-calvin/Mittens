cffa3b616d77104d3b4d8e79afeb3c6e09c83d01
im-calvin <kelvinwong0519@gmail.com>
2023-07-01 01:06:30 -0700

    set default migration to be true, add migration:revert trip flag to be true whenever video goes live 2/3

--- src/db/migration/1688194621864-AddLivePinged.ts
+++ src/db/migration/1688194621864-AddLivePinged.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddLivePinged1688194621864 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE videos ADD live_pinged boolean`);
+    await queryRunner.query(`ALTER TABLE videos ADD live_pinged boolean DEFAULT true`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
