94080de254e7afa4bc445f496acfaede2d1c0371
im-calvin <kelvinwong0519@gmail.com>
2023-07-01 14:29:45 -0700

    update livePinged migration

--- src/db/migration/1688194621864-AddLivePinged.ts
+++ src/db/migration/1688194621864-AddLivePinged.ts
@@ -2,7 +2,13 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddLivePinged1688194621864 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE videos ADD live_pinged boolean DEFAULT true`);
+    await queryRunner.query(`ALTER TABLE videos ADD live_pinged boolean`);
+    await queryRunner.query(
+      `UPDATE videos SET live_pinged = true WHERE scheduled_time < datetime()`
+    );
+    await queryRunner.query(
+      `UPDATE videos SET live_pinged = false WHERE scheduled_time > datetime()`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
